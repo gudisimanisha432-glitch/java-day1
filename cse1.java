26592fb79cbbd188765770a9c67a976821788ea6
gudisimanisha432-glitch <gudisimanisha432@gmail.com>
2026-01-31 14:29:24 +0530

    Add files via upload

---
+++ cse1.java
@@ -0,0 +1,10 @@
+class cse1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("computer engineering");
+        System.out.println("placements");
+        System.out.print("file");
+    {
+        
+}   
