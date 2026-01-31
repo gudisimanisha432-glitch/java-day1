a8a5a9b40f16b63c6c6830902912a3deb654f445
gudisimanisha432-glitch <gudisimanisha432@gmail.com>
2026-01-31 14:47:31 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,10 @@
+class Demo2
+{
+    public static void main(String[] args)
+    {
+     System.out.println(x:"welcome");
+    System.out.println(x:"mtiet");
+    System.out.print(A:"hello world");
+    }
+    
+}
