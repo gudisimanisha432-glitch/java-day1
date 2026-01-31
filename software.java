190124a3943f9cc4725c4c30f2e30929608fce99
gudisimanisha432-glitch <gudisimanisha432@gmail.com>
2026-01-31 15:04:40 +0530

    Add files via upload

---
+++ software.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class software
+{
+    public static void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        int age=obj.nextInt();
+        System.out.println(age);
+    }
+}
